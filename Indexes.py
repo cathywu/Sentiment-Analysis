422e94f25c74b3521da38fbf6e42a92e82357c21
Cathy Wu <neverisforever@gmail.com>
2012-01-15 07:18:08 -0500

    class that generates Indexes for different test configurations

---
+++ Indexes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import numpy
+
+class Indexes:
+    """
+    Indexes object generates indices for different configurations
+    Modes:
+    'r' : random
+    'd' : deterministic
+    'k' : k-fold cross-fold validation
+    """
+    def __init__(self):
+        self.mode = 'r'
+        self.iterations = 10
+        __init__('r',10,300)
+    def __init__(self,mode,iterations,train_size):
+        self.mode = mode
+        self.iterations = iterations
+        self.train_size = train_size
+        self.pos_train_ind = None 
+        self.pos_test_ind = None
+        self.neg_train_ind = None
+        self.neg_test_ind = None
+        self.gen_indices = generate_indices(mode,iterations,train_size)
+    def next(self):
+        (a,b,c,d) = self.gen_indices.next()
+        self.pos_train_ind = a 
+        self.pos_test_ind = b
+        self.neg_train_ind = c
+        self.neg_test_ind = d
+    def get_pos_train_ind(self):
+        return self.pos_train_ind
+    def get_pos_test_ind(self):
+        return self.pos_test_ind
+    def get_neg_train_ind(self):
+        return self.neg_train_ind
+    def get_neg_test_ind(self):
+        return self.neg_test_ind
+
+def shuffle_ind():
+    ind = numpy.arange(1000)
+    from numpy.random import shuffle
+    shuffle(ind)
+    return ind
+
+def generate_indices(mode='r',iterations=1,train_size=300):
+    if mode=='d': # deterministic
+        def get_indices():
+            ind = numpy.arange(1000)
+            pos_train_ind = ind[:train_size]
+            pos_test_ind = ind[train_size:]
+            neg_train_ind = ind[:train_size]
+            neg_test_ind = ind[train_size:]
+            for i in range(iterations):
+                yield (pos_train_ind, pos_test_ind, neg_train_ind, neg_test_ind)
+    elif mode=='r': # random
+        def get_indices():
+            for i in range(iterations):
+                pos_ind = shuffle_ind()
+                pos_train_ind = pos_ind[:train_size]
+                pos_test_ind = pos_ind[train_size:]
+                neg_ind = shuffle_ind()
+                neg_train_ind = neg_ind[:train_size]
+                neg_test_ind = neg_ind[train_size:]
+                yield (pos_train_ind, pos_test_ind, neg_train_ind, neg_test_ind)
+    elif mode=='k': # k-fold cross-validation
+        pass #TODO
+    return get_indices()
